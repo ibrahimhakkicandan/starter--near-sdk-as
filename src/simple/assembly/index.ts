d43674263247d0f1a0df9479714b0d3cf32f6dba
ibrahimhakkicandan <kartaliza113@gmail.com>
2022-05-01 14:38:45 -0400

    added comment line

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -4,7 +4,7 @@ import { storage, Context } from "near-sdk-as"
 export function helloWorld(names: Array<string>): string {
   return names.map<string>(name => 'hello ' + name).join(` `)
 }
-
+// changed something
 
 // read the given key from account (contract) storage
 export function read(key: string): string {
